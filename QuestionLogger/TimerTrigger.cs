a6601faf8ebb3594a51758c9c8d6864286170a65
kuro11 <kuro11@gmail.com>
2024-03-28 19:30:07 -0500

    chore: delivery notification spotty - just assume a queued message was sent

--- QuestionLogger/TimerTrigger.cs
+++ QuestionLogger/TimerTrigger.cs
@@ -14,7 +14,7 @@ namespace QuestionLogger;
 public static class TimerTrigger
 {
     [FunctionName("TimerTrigger")]
-    public static async Task RunAsync([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer, ILogger log)
+    public static async Task RunAsync([TimerTrigger("0 0 * * * *")] TimerInfo myTimer, ILogger log)
     {
         log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
 
@@ -26,24 +26,6 @@ public static class TimerTrigger
         }
 
         log.LogInformation("question: " + question);
-
-        using var service = FunctionHelper.GetSheetsService();
-        var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
-
-        var values = new List<IList<object>>
-        {
-            new List<object>
-            {
-                question, "-", "-", "-"
-            }
-        };
-        var updateRequest = service.Spreadsheets.Values.Append(new ValueRange
-        {
-            Values = values,
-        }, spreadsheetId, "Logs!A:D");
-        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
-
-        await updateRequest.ExecuteAsync();
 
         var accountSid = FunctionHelper.GetEnvironmentVariable("TwilioSid");
         var authToken = FunctionHelper.GetEnvironmentVariable("TwilioAuth");
@@ -59,6 +41,25 @@ public static class TimerTrigger
         };
 
         await MessageResource.CreateAsync(messageOptions);
+
+        using var service = FunctionHelper.GetSheetsService();
+        var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
+
+        var ctNow = FunctionHelper.GetCentralTimeNow();
+        var values = new List<IList<object>>
+        {
+            new List<object>
+            {
+                question, ctNow.ToString(FunctionHelper.DateFormat), "-", "-"
+            }
+        };
+        var updateRequest = service.Spreadsheets.Values.Append(new ValueRange
+        {
+            Values = values,
+        }, spreadsheetId, "Logs!A:D");
+        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+        await updateRequest.ExecuteAsync();
     }
 
     private static async Task<string> GetQuestion()
@@ -79,7 +80,7 @@ public static class TimerTrigger
 
             var timespan = parsed.ToTimeSpan();
             var difference = (timespan - ctNow.TimeOfDay).Duration();
-            if (difference < TimeSpan.FromMinutes(15 / 2.0))
+            if (difference < TimeSpan.FromMinutes(60 / 2.0))
             {
                 return question;
             }
