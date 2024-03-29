eb9118ea68eab088b528dfe291164315a41b757f
kuro11 <kuro11@gmail.com>
2024-03-28 19:15:19 -0500

    feat: replace ifttt with Twilio

---
+++ QuestionLogger/TimerTrigger.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Google.Apis.Sheets.v4;
+using Google.Apis.Sheets.v4.Data;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace QuestionLogger;
+
+public static class TimerTrigger
+{
+    [FunctionName("TimerTrigger")]
+    public static async Task RunAsync([TimerTrigger("0 */15 * * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+
+        var question = await GetQuestion();
+        if (string.IsNullOrWhiteSpace(question))
+        {
+            log.LogInformation("No question");
+            return;
+        }
+
+        log.LogInformation("question: " + question);
+
+        using var service = FunctionHelper.GetSheetsService();
+        var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
+
+        var values = new List<IList<object>>
+        {
+            new List<object>
+            {
+                question, "-", "-", "-"
+            }
+        };
+        var updateRequest = service.Spreadsheets.Values.Append(new ValueRange
+        {
+            Values = values,
+        }, spreadsheetId, "Logs!A:D");
+        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+
+        await updateRequest.ExecuteAsync();
+
+        var accountSid = FunctionHelper.GetEnvironmentVariable("TwilioSid");
+        var authToken = FunctionHelper.GetEnvironmentVariable("TwilioAuth");
+        var toPhone = FunctionHelper.GetEnvironmentVariable("ToPhoneNumber");
+        var fromPhone = FunctionHelper.GetEnvironmentVariable("FromPhoneNumber");
+
+        TwilioClient.Init(accountSid, authToken);
+
+        var messageOptions = new CreateMessageOptions(new PhoneNumber(toPhone))
+        {
+            From = new PhoneNumber(fromPhone),
+            Body = question
+        };
+
+        await MessageResource.CreateAsync(messageOptions);
+    }
+
+    private static async Task<string> GetQuestion()
+    {
+        using var service = FunctionHelper.GetSheetsService();
+        var ctNow = FunctionHelper.GetCentralTimeNow();
+        var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
+
+        var getData = await service.Spreadsheets.Values.Get(spreadsheetId, "Questions!A2:B30").ExecuteAsync();
+        foreach (var row in getData.Values)
+        {
+            var time = row[0].ToString();
+            var question = row[1].ToString();
+            if (!TimeOnly.TryParse(time, out var parsed))
+            {
+                continue;
+            }
+
+            var timespan = parsed.ToTimeSpan();
+            var difference = (timespan - ctNow.TimeOfDay).Duration();
+            if (difference < TimeSpan.FromMinutes(15 / 2.0))
+            {
+                return question;
+            }
+        }
+
+        return null;
+    }
+}
--- QuestionLogger/WebTrigger.cs
+++ QuestionLogger/WebTrigger.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Security.Cryptography;
-using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Google.Apis.Sheets.v4;
@@ -13,78 +9,89 @@ using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using Twilio.Security;
 
 namespace QuestionLogger;
 
 public static class WebTrigger
 {
-    [FunctionName("GetQuestion")]
-    public static async Task<IActionResult> GetSms([HttpTrigger(AuthorizationLevel.Function, "get", Route = "question")] HttpRequest req, ILogger log)
+    [FunctionName("Broke")]
+    public static async Task<IActionResult> ReportBroke([HttpTrigger(AuthorizationLevel.Function, "post", Route = "broke")] HttpRequest req, ILogger log)
     {
-        if (!IsAuthorized(req))
-        {
-            return new UnauthorizedResult();
-        }
-
-        using var service = FunctionHelper.GetSheetsService();
-        var ctNow = FunctionHelper.GetCentralTimeNow();
-        var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
-
-        var getData = await service.Spreadsheets.Values.Get(spreadsheetId, "Questions!A2:B30").ExecuteAsync();
-        foreach (var row in getData.Values)
-        {
-            var time = row[0].ToString();
-            var question = row[1].ToString();
-            if (!TimeOnly.TryParse(time, out var parsed))
-            {
-                continue;
-            }
-
-            var timespan = parsed.ToTimeSpan();
-            var difference = (timespan - ctNow.TimeOfDay).Duration();
-            if (difference < TimeSpan.FromMinutes(15))
-            {
-                return new ContentResult
-                {
-                    Content = JsonConvert.SerializeObject(new { value1 = question }),
-                    ContentType = "application/json",
-                    StatusCode = 200
-                };
-            }
-        }
-
-        return new NotFoundResult();
+        log.LogWarning(await req.ReadAsStringAsync());
+        return new OkResult();
     }
 
     [FunctionName("SmsSent")]
     public static async Task<IActionResult> ReportSentSms([HttpTrigger(AuthorizationLevel.Function, "post", Route = "sms-out")] HttpRequest req, ILogger log)
     {
-        if (!IsAuthorized(req))
+        var form = await req.ReadFormAsync();
+        if (!IsTwilioAuthorized(req, form))
         {
             return new UnauthorizedResult();
+        }
+
+        var status = form["MessageStatus"];
+        if (!string.Equals(status, "delivered"))
+        {
+            return new OkResult();
         }
 
         using var service = FunctionHelper.GetSheetsService();
         var ctNow = FunctionHelper.GetCentralTimeNow();
         var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
 
-        var question = await new StreamReader(req.Body).ReadToEndAsync();
-
-        var values = new List<IList<object>>
+        var emptyUpdateRequest = service.Spreadsheets.Values.Append(new ValueRange
         {
-            new List<object>
+            Values = new List<IList<object>>
             {
-                question, ctNow.ToString(FunctionHelper.DateFormat), "-", "-"
-            }
-        };
-        var updateRequest = service.Spreadsheets.Values.Append(new ValueRange
-        {
-            Values = values,
+                new List<object>()
+            },
         }, spreadsheetId, "Logs!A:D");
-        updateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
+        emptyUpdateRequest.ValueInputOption = SpreadsheetsResource.ValuesResource.AppendRequest.ValueInputOptionEnum.USERENTERED;
 
-        await updateRequest.ExecuteAsync();
+        var emptyUpdateResult = await emptyUpdateRequest.ExecuteAsync();
+        // when update is used without any data - the result points to the cell that would have been appended to
+        var rowNumber = int.Parse(Regex.Replace(emptyUpdateResult.Updates.UpdatedRange, "[^0-9]", string.Empty));
+
+        var sheetRef = await service.Spreadsheets.Get(spreadsheetId).ExecuteAsync();
+        var sheetId = sheetRef.Sheets.Single(x => x.Properties.Title == "Logs");
+        await service.Spreadsheets.BatchUpdate(new BatchUpdateSpreadsheetRequest
+        {
+            Requests = new List<Request>
+            {
+                new()
+                {
+                    UpdateCells = new UpdateCellsRequest
+                    {
+                        Fields = "*",
+                        Start = new GridCoordinate
+                        {
+                            ColumnIndex = 1,
+                            // row number - 2 because 0-index and we want last row with data
+                            RowIndex = rowNumber - 2,
+                            SheetId = sheetId.Properties.SheetId
+                        },
+                        Rows = new List<RowData>
+                        {
+                            new()
+                            {
+                                Values = new List<CellData>
+                                {
+                                    new()
+                                    {
+                                        UserEnteredValue = new ExtendedValue
+                                        {
+                                            StringValue = ctNow.ToString(FunctionHelper.DateFormat),
+                                        },
+                                    },
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }, spreadsheetId).ExecuteAsync();
 
         return new OkResult();
     }
@@ -92,13 +99,17 @@ public static class WebTrigger
     [FunctionName("SmsResponse")]
     public static async Task<IActionResult> ReportResponseSms([HttpTrigger(AuthorizationLevel.Function, "post", Route = "sms-in")] HttpRequest req, ILogger log)
     {
-        if (!IsAuthorized(req))
+        var form = await req.ReadFormAsync();
+        if (!IsTwilioAuthorized(req, form))
+        {
+            return new UnauthorizedResult();
+        }
+        if (!string.Equals(FunctionHelper.GetEnvironmentVariable("ToPhoneNumber"), form["From"]))
         {
             return new UnauthorizedResult();
         }
 
-        string answerToQuestion = await new StreamReader(req.Body).ReadToEndAsync();
-
+        var answerToQuestion = form["Body"];
         using var service = FunctionHelper.GetSheetsService();
         var ctNow = FunctionHelper.GetCentralTimeNow();
         var spreadsheetId = FunctionHelper.GetEnvironmentVariable("SheetId");
@@ -163,27 +174,19 @@ public static class WebTrigger
         }, spreadsheetId).ExecuteAsync();
 
         await emptyUpdateRequest.ExecuteAsync();
+
         return new OkResult();
     }
 
-    [FunctionName("Test")]
-    public static Task<IActionResult> Test([HttpTrigger(AuthorizationLevel.Function, "get", Route = "test")] HttpRequest req, ILogger log)
+    private static bool IsTwilioAuthorized(HttpRequest req, IFormCollection form)
     {
-        return Task.FromResult<IActionResult>(new OkResult());
-    }
-
-    private static bool IsAuthorized(HttpRequest req)
-    {
-        var header = req.Headers["API-KEY"];
-        if (string.IsNullOrWhiteSpace(header))
-        {
-            return false;
-        }
-        var expectedKey = FunctionHelper.GetEnvironmentVariable("ApiKey");
-        var key = RandomNumberGenerator.GetBytes(16);
-        var a = HMACSHA256.HashData(key, Encoding.UTF8.GetBytes(header));
-        var b = HMACSHA256.HashData(key, Encoding.UTF8.GetBytes(expectedKey));
-        return a.SequenceEqual(b);
-
+        //see: https://www.twilio.com/docs/usage/tutorials/how-to-secure-your-csharp-aspnet-core-app-by-validating-incoming-twilio-requests#use-filter-attribute-to-validate-twilio-requests
+        var authToken = FunctionHelper.GetEnvironmentVariable("TwilioAuth");
+        var host = FunctionHelper.GetEnvironmentVariable("Host");
+        var url  = $"{host}{req.Path}";
+        var twilioSignature  = req.Headers["X-Twilio-Signature"];
+        var parameters = form.ToDictionary(p => p.Key, p => p.Value.ToString());
+        var validator = new RequestValidator(authToken);
+        return validator.Validate(url, parameters, twilioSignature);
     }
 }
